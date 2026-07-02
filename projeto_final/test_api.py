9388dcc37444e5b17867d33514de14eb87138956
DANTON DE CAMPOS RODRIGUES <03801115003@senacrs.edu.br>
2026-07-01 21:48:10 -0300

    subindo relatorio

--- projeto_final/test_api.py
+++ projeto_final/test_api.py
@@ -7,16 +7,24 @@ urllib3.disable_warnings()
 url = "https://steamspy.com/api.php"
 
 params = {
-    "request": "appdetails",
-    "appid": 730
+    "request": "all",
+    "page": 1
 }
 
 response = requests.get(url, params=params, verify=False)
 
+print("URL final:", response.url)
 print("Status:", response.status_code)
 
 if response.status_code == 200:
     dados = response.json()
-    print(json.dumps(dados, indent=4, ensure_ascii=False))
+
+    print("Quantidade de jogos:", len(dados))
+
+    for appid, jogo in list(dados.items())[:10]:  # Exibe apenas os 10 primeiros jogos
+        
+        print(appid, "-", jogo.get("name"))
 else:
     print(response.text)
+
+print("Fim do teste da API.")
